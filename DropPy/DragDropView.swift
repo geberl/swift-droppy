f2b5a3b4b5a86359e5afb939dde861bd0ed90383
Günther Eberl <guenther@eberl.se>
2017-10-02 20:14:09 +0200

    More modern way of handling promised files
    This method is not deprecated, but was introduced in macOS 10.12 as the replacement for namesOfPromisedFilesDropped.
    
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- DropPy/DragDropView.swift
+++ DropPy/DragDropView.swift
@@ -51,19 +51,43 @@ class DragDropView: NSView {
     }
     
     override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
-
-        // TODO this needs more work
-//        if (sender.draggingPasteboard().types?.contains(NSPasteboard.PasteboardType(rawValue: "com.apple.pasteboard.promised-file-content-type")))! {
-//            print("promise is contained")
+        // FilePromises only work as long as the drag operation is being performed, not afterwards. FilePromises do
+        // not exist as actual files somewhere on the disk, instead the parent application manages them abstraced,
+        // probably in some internal database. It only writes them out when asked to. And this is what the function
+        // receivePromisedFiles does. The file are only guaranteed to exist after its reader function is called,
+        // not right here in performDragOperation.
+        // https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/DragandDrop/Tasks/DraggingFiles.html
+        
+        // More info: "What's New in Cocoa" presentation at WWDC 2016
+        // https://developer.apple.com/videos/play/wwdc2016/203/ (24:12 to 28:25)
+        // http://devstreaming.apple.com/videos/wwdc/2016/203x2w42att1kdzg1ce/203/203_whats_new_in_cocoa.pd (p 132)
+        
+        // Introduced in Sierra (macOS 10.12) which I require anyways -> nice coincidence.
+        // NOBODY in GitHub or the web has this implemented as of yet (2017-10-02) -> profit!
+        
+//        if let filePromises = sender.draggingPasteboard().readObjects(forClasses: [NSFilePromiseReceiver.self],
+//                                                                      options: nil) as? [NSFilePromiseReceiver] {
+//            let promiseOperationQueue: OperationQueue = OperationQueue()
+//            for filePromise in filePromises {
+//                print(filePromise)
+//                print(filePromise.fileNames)  // []
+//                print(filePromise.fileTypes)  // "public.jpeg"
 //
-//            let myFilePath: String = "/Users/guenther/Development/droppy-workspace/Temp/promises/"
-//            let createdFilePaths = sender.namesOfPromisedFilesDropped(atDestination: URL(fileURLWithPath: myFilePath))
-//            print(createdFilePaths ?? "nothing created, error")
-//        } else {
-//            print("promise is not contained")
+//                let myFilePath: String = "/Users/guenther/Development/droppy-workspace/Temp/promises/"
+//                filePromise.receivePromisedFiles(atDestination: URL(fileURLWithPath: myFilePath),
+//                                                 operationQueue: promiseOperationQueue,
+//                                                 reader: self.myReaderBlock)
+//            }
 //        }
         
         return true
+    }
+    
+    func myReaderBlock(myUrl: URL, myError: Error?) {
+        print("reader block called")
+        
+        print(myUrl.path)
+        print(isFile(path: myUrl.path))
     }
     
     override func concludeDragOperation(_ sender: NSDraggingInfo?) {
