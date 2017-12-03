22e943abce85dbe8feddd5c5968a99d9893310c5
Günther Eberl <guenther@eberl.se>
2017-12-03 11:43:25 +0100

    Got rid of XCode error window null
    Is an Xcode bug, worked around by creating empty classes
    
    Signed-off-by: Günther Eberl <guenther@eberl.se>

---
+++ DropPy/WindowPrefs.swift
@@ -0,0 +1,22 @@
+//
+//  WindowPrefs.swift
+//  DropPy
+//
+//  Created by Günther Eberl on 03.12.17.
+//  Copyright © 2017 Günther Eberl. All rights reserved.
+//
+
+import Cocoa
+
+class WindowPrefs: NSWindow {
+    
+    // Empty class created to get rid of XCode bug in 9.1:
+    // (only this message shows up on startup on the console, the app starts and works without problems)
+    
+    // 2017-12-03 11:06:16.138891+0100 DropPy[34991:11430223] Unknown Window class (null) in Interface Builder file,
+    // creating generic Window instead
+    
+    // Stack Exchange:
+    // https://stackoverflow.com/questions/47051682/unknown-window-class-null-in-interface-builder-file-creating-generic-window-i
+    
+}
